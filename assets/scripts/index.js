95f4440fbe22cb12829dc1ffce10c5b0495e4e1d
xKavai <jak.wisn99@gmail.com>
2022-07-19 14:42:27 +0200

    Update: added optimalization for results when typing

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,42 +1,49 @@
-//1. Jeżeli jest wpisane <4 - nie wyszukuje [DONE]
-//2. Przygotowanie struktury HTML do podpowiedzi
-//3. Sprawdź czy istnieje .tips (jeśli TAK, usuwamy .tips)
+//1. Przygotowanie struktury HTML do podpowiedzi [DONE]
+//2. Loaderek [X]
+//3. Jak zoptymalizować [DONE]
+//4. async wait - przeczytać [DONE/X]
+//5. Poczytaj jak zdobywać dane z linku [X]
+//6. Do punktu 5. zrób dodatkową stronę.
+//7. Jesli na dodatkowej stronie będzie w linku Luke+Skywalker, pobierz dane z ?search=Luke+Skywalker
 
 const search = document.getElementById('search1');
 const searchForm = document.getElementById('searchForm');
+const tableM = document.querySelector('.tableM');
+
+let wait = false;
 
 search.addEventListener(
     'keyup',
     (event) => {
         const value = event.target.value;
         if (value.length >= 4) {
-            fetch(`https://swapi.dev/api/people/?search=${value}`)
-            .then(res => res.json())
-                .then(data => {
-                    const tipsDiv = document.querySelector('.tips')
-                    if (tipsDiv)
-                        tipsDiv.remove();
+            const timer = setTimeout(() => {
+                if (!wait) {
+                    wait = true;
+                    fetch(`https://swapi.dev/api/people/?search=${value}`)
+                        .then(res => res.json())
+                        .then(data => {
+                            const tipsDiv = document.querySelector('.tips')
+                            if (tipsDiv)
+                                tipsDiv.remove();
 
-                    const tips = document.createElement('div');
-                    tips.classList.add('tips', 'd-flex')
-                    for (let i = 0; i < data.results.length; i++) {
-                        const tip = document.createElement('a');
-                        tip.href = "#";
-                        tip.innerHTML = data.results[i].name;
-                        
-                        tips.appendChild(tip)
-                        
-                        if (typeof tips != "undefined") {
-                            alert("Yep, it exists")
-                        }
-                            else {
-                                tips.remove();
-                        };
-                        console.log(data);
-                    };
-                    
-                    event.target.closest('form').appendChild(tips);
-            })
+                            const tips = document.createElement('div');
+                            tips.classList.add('tips', 'bg-light', 'd-flex', "flex-column", "border-top-0", "border-primary", "rounded-bottom")
+                            for (let i = 0; i < data.results.length; i++) {
+                                const tip = document.createElement('a');
+                                tip.href = "#";
+                                tip.innerHTML = data.results[i].name;
+                                
+                                tips.appendChild(tip)
+                                
+                                // console.log(data);
+                            };
+                            
+                            event.target.closest('form').appendChild(tips);
+                            wait = false;
+                        })
+                }
+            }, 1000)
         }
     }
 )
@@ -45,18 +52,42 @@ searchForm.addEventListener(
     'submit',
     (event) => {
         event.preventDefault();
-            const value = event.target.value;
-            if (value.length >= 4) {
-                fetch(`https://swapi.dev/api/people/?search=${value}`)
-                .then(res => res.json())
-                    .then(data => {
+            const value = search.value;
+            fetch(`https://swapi.dev/api/people/?search=${value}`)
+            .then(res => res.json())
+                .then(data => {
                         
-                        const sForm = document.createElement('table');
-                        sForm.classList.add('table-light', )
-                        for (let i = 0; i < data.results.length; )
-                        
+                    const searchDiv = tableM.querySelector('table')
+                    if (searchDiv)
+                        searchDiv.remove();
+
+                    const sForm = document.createElement('table');
+                    sForm.classList.add('table','table-light');
+                    console.log(data, data.results.length);
+                    
+                    const tHead = document.createElement('thead')
+                            
+                    tHead.innerHTML = `<tr><th scope="col">#</th><th scope="col;">Name</th><th scope="col">Height</th><th scope="col">Mass</th></tr>`;
+
+                    sForm.appendChild(tHead)
+                    
+                    const tBody = document.createElement('tbody');
+                    
+                    
+
+                    for (let i = 0; i < data.results.length; i++) {
+
+                        tBody.innerHTML += `<tr>
+                        <th scope="row">${i+1}</th>
+                        <td>${data.results[i].name}</td>
+                        <td>${data.results[i].height}</td>
+                        <td>${data.results[i].mass}</td>
+                    </tr>`
+                    };
+                        sForm.appendChild(tBody);
+                        tableM.appendChild(sForm);
                     })
-            }
+            
         
     }
 )
